525b90389e0123a50af312876e2fa8c608c3693f
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-10-14 14:24:51 +0200

    Update test

--- test/test.index.js
+++ test/test.index.js
@@ -1,9 +1,10 @@
 var assert = require('assert');
-var glitch = require('./../lib/glitch');
+var glitxt = require('./../lib/index');
 
-describe('lib/glitch.js', function() {
+describe('lib/index.js', function() {
 
-	describe('#encode', function() {
+
+  describe('#encode', function() {
 
 	});
 
@@ -11,13 +12,13 @@ describe('lib/glitch.js', function() {
   describe('#decode', function() {
 
   	it('should return the .', function() {
-    	glitch.decode.file('/files/hello-world.gif', function(data) {
+    	glitxt.decode.file('/files/hello-world.gif', function(data) {
     		assert.equal( 'test', data );
       });
     });
 
     it('should return the .', function() {
-    	glitch.decode.url('https://dl.dropboxusercontent.com/u/2874680/glitxt/generated_gifs/1380399254693.gif', function(data) {
+    	glitxt.decode.url('https://dl.dropboxusercontent.com/u/2874680/glitxt/generated_gifs/1380399254693.gif', function(data) {
     		assert.equal( 'test', data );
       });
     });
