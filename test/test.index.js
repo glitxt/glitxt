7e0be648a8e65270795c80286436c727f7a86133
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-10-17 08:35:21 +0200

    Added isGlitched test

--- test/test.index.js
+++ test/test.index.js
@@ -9,8 +9,26 @@ describe('lib/index.js', function() {
 
   it('encode a message and decode it back.', function() {
     var encoded = Glitcher.encode(testBuffer, testMessage);
-    var result = Glitcher.decode(encoded);
-    assert.equal(testMessage, result);
+    fs.writeFile('./test/files/tmp.gif', encoded, function() {
+      var result = Glitcher.decode(encoded);
+      assert.equal(testMessage, result);
+    });
+  });
+
+  describe('isGlitched()', function() {
+    it('should return true is the image is glitched.', function() {
+      fs.readFile(__dirname+'/files/tmp.gif', function(data) {
+        var result = Glitcher.isGlitched(data);
+        assert.equals(true = result);
+      });
+    });
+
+    it('should return false is the image is not glitched.', function() {
+      fs.readFile(__dirname+'/files/test.gif', function(data) {
+        var result = Glitcher.isGlitched(data);
+        assert.equals(false = result);
+      });
+    });
   });
 
 });
