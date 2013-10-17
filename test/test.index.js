d4fa512f6b597015eae5f066d89febbfc6d5accd
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-10-17 11:04:55 +0200

    Update test

--- test/test.index.js
+++ test/test.index.js
@@ -7,23 +7,25 @@ var testMessage = 'hello-world';
 
 describe('lib/index.js', function() {
 
-  it('encode a message and decode it back.', function() {
-    var encoded = Glitcher.encode(testBuffer, testMessage);
-    fs.writeFile('./test/files/tmp.gif', encoded, function() {
-      var result = Glitcher.decode(encoded);
-      assert.equal(testMessage, result);
+  describe('encode(), decode()', function() {
+    it('encode a message and decode it back.', function() {
+      var encoded = Glitcher.encode(testBuffer, testMessage);
+      fs.writeFile('./test/files/tmp.gif', encoded, function() {
+        var result = Glitcher.decode(encoded);
+        assert.equal(testMessage, result);
+      });
     });
   });
 
   describe('isGlitched()', function() {
-    it('should return true is the image is glitched.', function() {
+    it('should return true if the image is glitched.', function() {
       fs.readFile(__dirname+'/files/tmp.gif', function(data) {
         var result = Glitcher.isGlitched(data);
         assert.equals(true = result);
       });
     });
 
-    it('should return false is the image is not glitched.', function() {
+    it('should return false if the image is not glitched.', function() {
       fs.readFile(__dirname+'/files/test.gif', function(data) {
         var result = Glitcher.isGlitched(data);
         assert.equals(false = result);
